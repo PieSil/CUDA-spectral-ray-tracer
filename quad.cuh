8d4ad441dda4679039b65afa708ee3d1b2c3009e
Pietro Siliani <s314518@studenti.polito.it>
2024-04-22 12:30:02 +0200

    Added box struct in order to represent 3D boxes

--- quad.cuh
+++ quad.cuh
@@ -74,5 +74,103 @@ public:
     vec3 w; //cached value needed during ray intersection computation
 };
 
+struct box {
+    __device__
+    box(const point3& a, const point3& b, material* mat, quad** _sides) {
+        point3 min = point3(fmin(a.x(), b.x()), fmin(a.y(), b.y()), fmin(a.z(), b.z()));
+        point3 max = point3(fmax(a.x(), b.x()), fmax(a.y(), b.y()), fmax(a.z(), b.z()));
+
+        vec3 dx = vec3(max.x() - min.x(), 0.f, 0.f);
+        vec3 dy = vec3(0, max.y() - min.y(), 0.f);
+        vec3 dz = vec3(0, 0, max.z() - min.z());
+
+        _sides[0] = new quad(point3(min.x(), min.y(), max.z()), dx, dy, mat); // front
+        _sides[1] = new quad(point3(max.x(), min.y(), max.z()), -dz, dy, mat); // right
+        _sides[2] = new quad(point3(max.x(), min.y(), min.z()), -dx, dy, mat); // back
+        _sides[3] = new quad(point3(min.x(), min.y(), min.z()), dz, dy, mat); // left
+        _sides[4] = new quad(point3(min.x(), max.y(), max.z()), dx, -dz, mat); // top
+        _sides[5] = new quad(point3(min.x(), min.y(), min.z()), dx, dz, mat); // bottom
+        sides = _sides;
+    }
+
+    __device__
+    box(quad** _sides) {
+        sides = _sides;
+    }
+
+    __device__
+    box(const point3 center, vec3 width_vec, vec3 height_vec, vec3 depth_vec, material* mat, quad** _sides) {
+        vec3 half_sum = (width_vec + height_vec + depth_vec) / 2.0f;
+        point3 min = center - half_sum;
+
+        //point3 min = point3(fmin(a.x(), b.x()), fmin(a.y(), b.y()), fmin(a.z(), b.z()));
+        //point3 max = point3(fmax(a.x(), b.x()), fmax(a.y(), b.y()), fmax(a.z(), b.z()));
+
+        _sides[0] = new quad(point3(min.x(), min.y(), min.z()) + depth_vec, width_vec, height_vec, mat); // front
+        _sides[1] = new quad(point3(min.x(), min.y(), min.z()) + depth_vec + width_vec, -depth_vec, height_vec, mat); // right
+        _sides[2] = new quad(point3(min.x(), min.y(), min.z()) + width_vec, -width_vec, height_vec, mat); // back
+        _sides[3] = new quad(point3(min.x(), min.y(), min.z()), depth_vec, height_vec, mat); // left
+        _sides[4] = new quad(point3(min.x(), min.y(), min.z()) + height_vec + depth_vec, width_vec, -depth_vec, mat);// top
+        _sides[5] = new quad(point3(min.x(), min.y(), min.z()), width_vec, depth_vec, mat); // bottom
+
+        sides = _sides;
+    }
+
+    __device__
+    box(const box& outer, material* mat, quad** _sides, const float offset = 0.0f) {
+        
+        vec3 outer_width_vec = outer.getWidthVec();
+        vec3 outer_height_vec = outer.getHeightVec();
+        vec3 outer_depth_vec = outer.getDepthVec();
+
+
+        box(outer.getCenter(), outer_width_vec - offset * unit_vector(outer_width_vec), outer_height_vec - offset * unit_vector(outer_height_vec), outer_depth_vec - offset * unit_vector(outer_depth_vec), mat, _sides);
+        sides = _sides;
+    }
+
+    __host__ __device__
+    const point3 getLocalMin() const {
+        return sides[5]->Q;
+    }
+
+    __host__ __device__
+    const point3 getLocalMax() const {
+        point3 min = getLocalMin();
+        return min + getWidthVec() + getHeightVec() + getDepthVec();
+        //return point3(sides[4]->Q.e + sides[4]->u, sides[4]->Q.e[1], sides[4]->Q.e[2]);
+    }
+
+    __host__ __device__
+        const point3 getLocalMax(const point3 min) const {
+        return min + getWidthVec() + getHeightVec() + getDepthVec();
+        //return point3(sides[4]->Q.e + sides[4]->u, sides[4]->Q.e[1], sides[4]->Q.e[2]);
+    }
+
+    __host__ __device__
+    const point3 getCenter() const {
+        point3 min = getLocalMin();
+        point3 max = getLocalMax(min);
+        point3 center = (max - min) / 2.0f + min;
+        return center;
+    }
+
+    __host__ __device__
+    const vec3 getWidthVec() const {
+        return sides[5]->u;
+    }
+
+    __host__ __device__
+    const vec3 getHeightVec() const {
+        return sides[3]->v;
+    }
+
+    __host__ __device__
+    const vec3 getDepthVec() const {
+        return sides[5]->v;
+    }
+
+    quad** sides;
+};
+
 
 #endif //RTWEEKEND_CUDA_QUAD_CUH
