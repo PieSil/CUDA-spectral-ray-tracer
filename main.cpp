494c2b1271ad97eb3f77a616d436d16e0d115da9
“PieSil” <s314518@studenti.polito.it>
2024-05-14 14:34:24 +0200

    Moved render loop in separate function

--- main.cpp
+++ main.cpp
@@ -10,6 +10,38 @@
 
 using namespace scene;
 
+bool render_loop(render_manager& rm, frame_buffer& fb, uchar_img& dst_image, img_display& disp, image_channels& ch) {
+    bool completed = false;
+
+    if (rm.isReadyToRender()) {
+        disp.display(dst_image);
+
+        clock_t start, stop;
+        start = clock();
+
+        std::clog << "Rendering... ";
+
+        while (!rm.isDone()) {
+            rm.step();
+            rm.update_fb();
+            ch = fb;
+            dst_image = get_image(ch.r, ch.g, ch.b, rm.getImWidth(), rm.getImHeight());
+            disp.render(dst_image);
+            disp.paint();
+        }
+
+        stop = clock();
+        double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
+        std::clog << "done, took " << timer_seconds << " seconds.\n";
+        completed = true;
+    }
+    else {
+        cerr << "Device parameters not yet initialized";
+    }
+
+    return completed;
+}
+
 int main() {
     init_device_symbols();
     
@@ -23,18 +55,13 @@ int main() {
         image_channels ch(fb);
         uchar_img image = get_image(ch.r, ch.g, ch.b, width, height);
         img_display disp = get_image_display("Render");
-        disp.display(image);
 
         render_manager rm(sm.getWorld(), sm.getCamPtr(), fb.data);
 
         rm.init_renderer(BOUNCE_LIMIT, SAMPLES_PER_PIXEL);
         rm.init_device_params(100, 100);
 
-        clock_t start, stop;
-        start = clock();
-
-        std::clog << "Rendering... ";
-
+        /*
         while (!rm.isDone()) {
             rm.step();
             rm.update_fb();
@@ -43,11 +70,7 @@ int main() {
             disp.render(image);
             disp.paint();
         }
-
-
-        stop = clock();
-        double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
-        std::clog << "done, took " << timer_seconds << " seconds.\n";
+        */
         
         /*
         ch = fb;
@@ -55,16 +78,19 @@ int main() {
         disp.render(image);
         disp.paint();
         */
-        
-        save_img(image, "test.bmp");
-        while (!disp.is_closed()) {
-            disp.wait();
+       
+        if (render_loop(rm, fb, image, disp, ch)) {
+            save_img(image, "test.bmp");
+            while (!disp.is_closed()) {
+                disp.wait();
+            }
         }
-        
-
+       
         //write_to_ppm(fb.data, width, height);
     } else {
         cerr << res.msg << endl;
     }
     return 0;
 }
+
+
