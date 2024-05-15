2c1996b9d8bd39d11d5c3a0079a903150d6de038
“PieSil” <s314518@studenti.polito.it>
2024-05-15 11:19:09 +0200

    Refactored code in order to use multithreading

--- main.cpp
+++ main.cpp
@@ -5,14 +5,13 @@
 #include "image.h"
 #include "render_manager.cuh"
 
-#define SAMPLES_PER_PIXEL 100
+#define SAMPLES_PER_PIXEL 1000
 #define BOUNCE_LIMIT 10
 
 using namespace scene;
 
-bool render_loop(render_manager& rm, frame_buffer& fb, uchar_img& dst_image, img_display& disp, image_channels& ch) {
+bool start_render(render_manager& rm, frame_buffer& fb, uchar_img& dst_image, img_display& disp, image_channels& ch, bool multithread = true) {
     bool completed = false;
-
     if (rm.isReadyToRender()) {
         disp.display(dst_image);
 
@@ -20,19 +19,36 @@ bool render_loop(render_manager& rm, frame_buffer& fb, uchar_img& dst_image, img
         start = clock();
 
         std::clog << "Rendering... ";
+        
+        if (multithread) {
+            rm.start_render();
+            bool has_data = true;
 
-        while (!rm.isDone()) {
-            rm.step();
-            rm.update_fb();
-            ch = fb;
-            dst_image = get_image(ch.r, ch.g, ch.b, rm.getImWidth(), rm.getImHeight());
-            disp.render(dst_image);
-            disp.paint();
+            do {
+                has_data = rm.update_fb();
+                ch = fb;
+                dst_image = get_image(ch.r, ch.g, ch.b, rm.getImWidth(), rm.getImHeight());
+                disp.render(dst_image);
+                disp.paint();
+            } while (has_data);
+
+        }
+        else {
+            bool has_data = true;
+            do {
+                has_data = rm.step();
+                rm.update_fb();
+                ch = fb;
+                dst_image = get_image(ch.r, ch.g, ch.b, rm.getImWidth(), rm.getImHeight());
+                disp.render(dst_image);
+                disp.paint();
+            } while (has_data);
         }
 
         stop = clock();
         double timer_seconds = ((double)(stop - start)) / CLOCKS_PER_SEC;
         std::clog << "done, took " << timer_seconds << " seconds.\n";
+        rm.end_render();
         completed = true;
     }
     else {
@@ -42,9 +58,7 @@ bool render_loop(render_manager& rm, frame_buffer& fb, uchar_img& dst_image, img
     return completed;
 }
 
-int main() {
-    init_device_symbols();
-    
+void render_cycle(bool multithread = true) {
     scene_manager sm = scene_manager();
     result res = sm.getResult();
     if (res.success) {
@@ -59,7 +73,7 @@ int main() {
         render_manager rm(sm.getWorld(), sm.getCamPtr(), fb.data);
 
         rm.init_renderer(BOUNCE_LIMIT, SAMPLES_PER_PIXEL);
-        rm.init_device_params(100, 100);
+        rm.init_device_params(200, 200);
 
         /*
         while (!rm.isDone()) {
@@ -71,25 +85,38 @@ int main() {
             disp.paint();
         }
         */
-        
+
         /*
         ch = fb;
         image = get_image(ch.r, ch.g, ch.b, width, height);
         disp.render(image);
         disp.paint();
         */
-       
-        if (render_loop(rm, fb, image, disp, ch)) {
+
+        if (start_render(rm, fb, image, disp, ch, multithread)) {
+            /*
             save_img(image, "test.bmp");
             while (!disp.is_closed()) {
                 disp.wait();
             }
+            */;
         }
-       
+
         //write_to_ppm(fb.data, width, height);
-    } else {
+    }
+    else {
         cerr << res.msg << endl;
     }
+}
+
+int main() {
+    init_device_symbols();
+    //clog << "Multithread" << endl;
+    render_cycle();
+
+    //clog << "Single thread" << endl;
+    //render_cycle(false);
+    
     return 0;
 }
 
