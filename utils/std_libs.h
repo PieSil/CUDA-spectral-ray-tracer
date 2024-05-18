6dafc0b5654291f27632d5532e667cddbcdbb838
“PieSil” <s314518@studenti.polito.it>
2024-05-18 12:03:40 +0200

    Changed extension to .h

---
+++ utils/std_libs.h
@@ -0,0 +1,14 @@
+//
+// Created by pietr on 03/04/2024.
+//
+
+#ifndef SPECTRAL_RT_PROJECT_STD_LIBS_H
+#define SPECTRAL_RT_PROJECT_STD_LIBS_H
+
+#include <iostream>
+#include <cmath>
+#include <random>
+#include <limits>
+#include <ctime>
+
+#endif //SPECTRAL_RT_PROJECT_STD_LIBS_CUH
